7a56199a46f840b3f9d659a985212a0efd60471a
khadir <tolibxonovm@gmail.com>
2024-12-23 08:26:30 +0500

    set one bg image

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -3,9 +3,10 @@ import { auth } from "../firebase/config";
 
 export const useLogin = () => {
   const loginWithEmailAndPassword = (email, password) => {
+    console.log(email, password);
     signInWithEmailAndPassword(auth, email, password)
       .then((profile) => {
-        console.log(profile.user);
+        console.log(profile);
       })
       .catch((error) => {
         console.log(error.message);
